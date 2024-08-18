3dc6c0d700fe251e35bf1ecb8896ff672b90acdf
Chan0129 <142906874+Chan0129@users.noreply.github.com>
2024-08-18 22:16:17 +0800

    update

---
+++ contact.js
@@ -0,0 +1,47 @@
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  if (!contact) {
+    return null;
+  }
+  return contact;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const newList = contacts.filter((contact) => contact.id !== contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(newList));
+  const removeContact = contacts.find((contacts) => contact.id === contactId);
+  return removeContact;
+};
+
+const addContact = async (id, name, email, phone) => {
+  const newContact = {
+    id: nanoid(7),
+    name,
+    email,
+    phone,
+  };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,51 @@
+const { program } = require("commander");
+program
+  .option("-a, --action<type>", "contacts operation")
+  .option("-i, --id<type>", "contact id")
+  .option("-n, --name <type>", "contact name")
+  .option("-e, --email <type>", "contact email")
+  .option("-p, --phones <type>", "contact phone");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+const { nanoid } = require("nanoid");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const list = await listContacts();
+      console.table(list);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      if (!contact) {
+        throw new Error(`contact with id =${id} does not exist`);
+      }
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await addContact(id, name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removedContact = await removeContact(id);
+      if (!removeContact) {
+        throw new Error(`contact with id=${id}does not exist`);
+      }
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unkwon action type!");
+  }
+};
+program.parse(process.argv);
+const options = program.opts();
+
+invokeAction(options);
